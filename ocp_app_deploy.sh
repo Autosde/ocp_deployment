daadff5316579ac6ca08c33c728f6aa83ab3162d
felixnu <70142655+felixnu@users.noreply.github.com>
2021-03-22 17:08:18 +0200

    Update ocp_app_deploy.sh

--- ocp_app_deploy.sh
+++ ocp_app_deploy.sh
@@ -298,7 +298,7 @@ else
     cat > service_patch.json << EOF
 [{"op":"replace","path":"/spec","value":{"ports":[{"name":"http","protocol":"TCP","port":3000,"targetPort":3000,}],"selector":{"app":"pythonflaskfelix"},"type":"ClusterIP","clusterIP":"$CLUSTER_IP","sessionAffinity":"None"}}]
 EOF
-    kubectl patch svc pythonflaskfelix --type='json' --patch  $(cat service_patch)
+    kubectl patch svc pythonflaskfelix --type='json' --patch  $(cat service_patch.json)
     echo -e "SERVICE: ${APP_SERVICE}"
     echo "DEPLOYED SERVICES:"
     kubectl describe services ${APP_SERVICE} --namespace ${CLUSTER_NAMESPACE}
