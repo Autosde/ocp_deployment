6167ed60adfeedcabd3419cf3fcc025efd5d177a
Felix Nutenko <felixnu@il.ibm.com>
2021-03-31 16:38:02 +0300

    Trivial changes

--- ocp_app_deploy.sh
+++ ocp_app_deploy.sh
@@ -300,10 +300,10 @@ else
   if [ ! -z "${APP_SERVICE}" ]; then
     CLUSTER_IP=$(kubectl get services -o jsonpath="{.items[?(@.metadata.name=='"${APP_SERVICE}"')].spec.clusterIP}")
     echo "ClusterIP: ${CLUSTER_IP}"
-cat > service_patch.json << EOF
-[{"op":"replace","path":"/spec","value":{"ports":[{"name":"http","protocol":"TCP","port":3000,"targetPort":3000,}],"selector":{"app":"${APP_SERVICE}"},"type":"ClusterIP","clusterIP":"$CLUSTER_IP","sessionAffinity":"None"}}]
-EOF
-    kubectl patch svc "${APP_SERVICE}" --type='json' --patch $(cat service_patch.json)
+#cat > service_patch.json << EOF
+#[{"op":"replace","path":"/spec","value":{"ports":[{"name":"http","protocol":"TCP","port":3000,"targetPort":3000,}],"selector":{"app":"${APP_SERVICE}"},"type":"ClusterIP","clusterIP":"$CLUSTER_IP","sessionAffinity":"None"}}]
+#EOF
+    #kubectl patch svc "${APP_SERVICE}" --type='json' --patch $(cat service_patch.json)
     echo -e "SERVICE: ${APP_SERVICE}"
     echo "DEPLOYED SERVICES:"
     kubectl describe services ${APP_SERVICE} --namespace ${CLUSTER_NAMESPACE}
